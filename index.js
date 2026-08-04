a1482625206af8b292e8ecc39e4cac3b38b1f4de
Sam Verneuille <samvernel268@gmail.com>
2026-08-04 11:03:40 -0400

    fix js error

---
+++ index.js
@@ -0,0 +1,18 @@
+// template_t0i67u1
+// 
+// XtYd7cGfTqAl0utMG
+function contact(event) {
+    event.preventDefault();
+//     emailjs
+//     .sendForm(
+//         'service_wdekawv',
+//         'template_t0i67u1',
+//         event.target,
+//         'user_XtYd7cGfTqAl0utMG'
+//     ).then(() => {
+//     console.log('it worked1')
+// })
+
+setTimeout(() => {
+    console.log('it worked 1')
+}
