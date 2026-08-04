9d40b3f60cb704ab36b55d9608a60ff31eda7c51
Sam Verneuille <samvernel268@gmail.com>
2026-08-04 11:51:08 -0400

    fix email not working

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 // template_t0i67u1
-// 
+//
 // XtYd7cGfTqAl0utMG
-function contact(event) {
-    event.preventDefault();
-//     emailjs
-//     .sendForm(
-//         'service_wdekawv',
-//         'template_t0i67u1',
-//         event.target,
-//         'user_XtYd7cGfTqAl0utMG'
-//     ).then(() => {
-//     console.log('it worked1')
-// })
 
-setTimeout(() => {
-    console.log('it worked 1')
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_wdekawv",
+      "template_t0i67u1",
+      event.target,
+      "user_XtYd7cGfTqAl0utMG",    
+    ).then(() => {
+       loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible")
+   alert(
+    "The email service is temporarily unavailable. Please contact me directly at samvernel268@gmail.com"
+   )
+})
 }
