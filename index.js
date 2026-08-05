c495b90790ecf213e39e44f17068637a7b869c83
Sam Verneuille <samvernel268@gmail.com>
2026-08-05 19:37:45 -0400

    fix modal toggle

--- index.js
+++ index.js
@@ -6,21 +6,25 @@ function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
-loading.classList += " modal__overlay--visible";
+  loading.classList += " modal__overlay--visible";
 
   emailjs
     .sendForm(
-      "service_wdekawv",
-      "template_t0i67u1",
-      event.target,
-      "user_XtYd7cGfTqAl0utMG",    
-    ).then(() => {
-       loading.classList.remove("modal__overlay--visible");
+        'service_wdekawv', 
+        'template_t0i67u1', 
+        event.target
+    )    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
       success.classList += " modal__overlay--visible";
-    }).catch(() => {
-      loading.classList.remove("modal__overlay--visible")
-   alert(
-    "The email service is temporarily unavailable. Please contact me directly at samvernel268@gmail.com"
-   )
-})
+    }) .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at samvernel268@gmail.com",
+      );
+    });
+}
+
+function toggleModal() {
+    // toggle modal
+    document.body.classList += " modal--open";
 }
