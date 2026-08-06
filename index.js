149a7301f890a775e875dbc8f277f2cd6c695964
Sam Verneuille <samvernel268@gmail.com>
2026-08-06 16:57:31 -0400

    setup dark mode

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// template_t0i67u1
-//
-// XtYd7cGfTqAl0utMG
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+document.body.classList += "dark-theme";
+}
+else{
+    document.body.classList.remove("dark-theme");
+}
 
 function contact(event) {
   event.preventDefault();
