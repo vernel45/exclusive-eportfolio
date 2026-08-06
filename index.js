d3da8526cf98b603ace48bd41a3a64389af94559
Sam Verneuille <samvernel268@gmail.com>
2026-08-06 16:16:21 -0400

    updated

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ function contact(event) {
 
 let isModalOpen = false;
 function toggleModal() {
-       if (isModalOpen) {
-        isModalOpen = !isModalOpen;
-        return document.body.classList.remove("modal--open");
-    } 
+  isModalOpen = !isModalOpen;
   document.body.classList.toggle("modal--open");
 }
-
